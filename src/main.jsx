4d65caa657f692d1b48b242ca84b5d2037876789
edanv1401 <edwis963@gmail.com>
2026-02-16 08:22:43 -0500

    feat: Add GitHub Pages SPA redirect script to `index.html` and initialize React app with routing, theming, and context providers in `main.jsx`.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './components/ThemeProvider'
 import { ProductProvider } from './context/ProductContext'
@@ -10,7 +10,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <AuthProvider>
           <ProductProvider>
@@ -20,6 +20,6 @@ createRoot(document.getElementById('root')).render(
           </ProductProvider>
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
